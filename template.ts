911b304f9822e914f8401f6f2035a41a2a79e31b
dqn <dqn270@gmail.com>
2023-02-10 07:02:49 +0900

    chore: update template

--- template.ts
+++ template.ts
@@ -1,7 +1,19 @@
 import { readFileSync } from "fs";
 
+function println(val: Object): void {
+  console.log(val.toString());
+}
+
 function input(): string {
   return readFileSync("/dev/stdin", "utf8").trim();
+}
+
+function bigIntMax(...args: bigint[]): bigint {
+  return args.reduce((m, e) => (e > m ? e : m));
+}
+
+function bigIntMin(...args: bigint[]): bigint {
+  return args.reduce((m, e) => (e < m ? e : m));
 }
 
 function main() {
