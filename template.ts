7329689a16e2fc6e581efa298215e4cb9a525e78
dqn <dqn270@gmail.com>
2023-02-10 07:59:22 +0900

    chore: update template

--- template.ts
+++ template.ts
@@ -4,8 +4,20 @@ function println(val: Object): void {
   console.log(val.toString());
 }
 
-function input(): string {
-  return readFileSync("/dev/stdin", "utf8").trim();
+function inputs(): string[][] {
+  return readFileSync("/dev/stdin", "utf8")
+    .trim()
+    .replace(/\r/g, "")
+    .split("\n")
+    .map((x) => x.split(" "));
+}
+
+function makeArray<T>(value: T, len: number): T[] {
+  return [...Array(len)].map(() => value);
+}
+
+function makeArray2<T>(value: T, lenX: number, lenY: number): T[][] {
+  return [...Array(lenY)].map(() => [...Array(lenX)].map(() => value));
 }
 
 function bigIntMax(...args: bigint[]): bigint {
@@ -16,8 +28,6 @@ function bigIntMin(...args: bigint[]): bigint {
   return args.reduce((m, e) => (e < m ? e : m));
 }
 
-function main() {
-  
-}
+function main() {}
 
 main();
